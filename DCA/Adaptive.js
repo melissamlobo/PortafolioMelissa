6b7a2506abc5560bd3b11cb14776f7e20e4dea7d
melissamlobo <melissamlobo@gmail.com>
2023-06-01 13:02:44 -0500

    nuevo commit

---
+++ DCA/Adaptive.js
@@ -0,0 +1,17 @@
+// CICLO FOR PARA AGREGAR EL NUMERO DE SAMPLES COMO KEYID Y SUS DESCRIPCIONES
+
+//var samples = new Array();
+
+con
+const samplesid = "";
+const description = "";
+const linkids = "";
+const altOUM = ""; //preguntar
+
+for (let i = 0; i < altOUM.size(); i++) {
+    samplesid += samplesid + ";";
+    description += description + ";";
+    linkids = "SDIGroupItem" //preguntar
+
+}
+
